86a48b602e0e551c7d5865d603347f5a0a115dad
elliotforbes <me@elliotf.dev>
2025-04-13 21:17:08 +0100

    Adding test to verify path params work

--- fakes_test.go
+++ fakes_test.go
@@ -1,7 +1,9 @@
 package fakes_test
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/elliotforbes/fakes"
@@ -36,6 +38,31 @@ func TestFakes(t *testing.T) {
 		fakeServer.Run(t)
 		t.Logf("BaseURL: %s\n", fakeServer.BaseURL)
 		request, err := http.NewRequest(http.MethodGet, fakeServer.BaseURL, nil)
+		assert.Nil(t, err)
+
+		response, err := http.DefaultClient.Do(request)
+		assert.Nil(t, err)
+
+		assert.Equal(t, http.StatusOK, response.StatusCode)
+	})
+
+	t.Run("test path parameters work as expected", func(t *testing.T) {
+		fakeServer := fakes.NewFakeHTTP()
+		fakeServer.AddEndpoint(&fakes.Endpoint{
+			Path:     "/:id",
+			Response: "{}",
+			Expectation: func(r *http.Request) {
+				id := strings.TrimPrefix(r.URL.Path, "/")
+				assert.Equal(t, "some-id", id)
+			},
+		})
+		fakeServer.Run(t)
+
+		request, err := http.NewRequest(
+			http.MethodGet,
+			fmt.Sprintf("%s/some-id", fakeServer.BaseURL),
+			nil,
+		)
 		assert.Nil(t, err)
 
 		response, err := http.DefaultClient.Do(request)
